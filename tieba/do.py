90454b035fa3b8895ea85c47758918e92124bbfc
zzde <zhangxh1997@gmail.com>
2017-12-01 14:44:12 +0800

    多线程签到 // 占用待提高

--- tieba/do.py
+++ tieba/do.py
@@ -3,38 +3,20 @@ from datetime import datetime
 from .models import UserProfile
 from .models import TiebaList
 from .util import get_tbs, do_sign
+from threading import Thread
 # from multiprocessing import Pool
 
 
-def do_sign_user(user_id):
-    """do_sign_user
-    params:
-        user_id
-    return:
-        res_code
-        0 done
-        1 部分失败
-    """
-    res_code       = 0
-    to_sign_tiebas = TiebaList.objects.filter(
-        Q(user_id=user_id), Q(is_sign__lt=datetime.now()))
-
-    if len(to_sign_tiebas)==0:
-        return res_code
-
-    user_bduss = UserProfile.objects.get(user_id=user_id).bduss
-    user_tbs   = get_tbs(user_bduss)
-
-    for to_sign_tieba in to_sign_tiebas:
-        # print(to_sign_tieba.tiebaname)
+def thread_sign(user_tbs, user_bduss, tiebas):
+    for to_sign_tieba in tiebas:
+        # print (to_sign_tieba.fid,to_sign_tieba.tiebaname)
         info = do_sign(user_tbs, user_bduss, to_sign_tieba.fid,
                        to_sign_tieba.tiebaname)
 
-        print (info)
+        print(info)
         error_code = info['error_code']
         to_sign_tieba.error_code = error_code
 
-        # print(info)
         # 没有消息就是最好的消息
         """error_code
             0      => 成功
@@ -49,17 +31,64 @@ def do_sign_user(user_id):
                 to_sign_tieba.error_msg = info['error_msg']
             to_sign_tieba.is_sign = datetime.now()
         else:
-            res_code                = 1
+            res_code = 1
             to_sign_tieba.error_msg = info['error_msg']
 
         to_sign_tieba.save()
+    # print ()
+
+
+def do_sign_user(user_id):
+    """do_sign_user
+    params:
+        user_id
+    return:
+        res_code
+        0 done
+        1 部分失败
+    """
+    res_code = 0
+    to_sign_tiebas = TiebaList.objects.filter(
+        Q(user_id=user_id), Q(is_sign__lt=datetime.now()))
+
+    if len(to_sign_tiebas) == 0:
+        return res_code
+
+    user_bduss = UserProfile.objects.get(user_id=user_id).bduss
+    user_tbs = get_tbs(user_bduss)
+
+    splist = lambda l: [l[i:i + 20] for i in range(len(l)) if i % 20 == 0]
+
+    # print (len(splist(to_sign_tiebas)))
+    # for to_sign_tieba in to_sign_tiebas:
+    #     # print(to_sign_tieba.tiebaname)
+    #     info = do_sign(user_tbs, user_bduss, to_sign_tieba.fid,
+    #                    to_sign_tieba.tiebaname)
+
+    #     print (info)
+    Threads = list()
+    for Tmps in splist(to_sign_tiebas):
+        # print (Tmps)
+        Threads.append(Thread(target=thread_sign,
+                       args=(user_tbs, user_bduss, Tmps)))
+    for t in Threads:
+        t.start()
+    for t in Threads:
+        t.join()
 
     return res_code
 
 
 def doo():
+    """
+        进程池 pool > 5 
+        todo 用户过多 过滤 标记用户
+    """
+
     all_users = UserProfile.objects.filter(~Q(bduss='null'))
+
     for tmp in all_users:
         do_sign_user(tmp.user_id)
 
     # print('1')
+
